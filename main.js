bc02e28a14f8527337455713c4ed30885c995448
AlexaDob <58271470+AlexaDob@users.noreply.github.com>
2022-06-23 11:53:35 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -21,15 +21,35 @@ var ball = {
     dy:3
 }
 
-function setup(){
-  var canvas =  createCanvas(700,600);
+function setup() {
+  var canvas = createCanvas(700, 600);
+  canvas.parent('canvas');
+
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide()
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+  console.log('Model Loaded!')
+}
+
+function gotPoses(results){
+  if(results.length > 0){
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +", noseY = " +noseY);
+  }
 }
 
 
 function draw(){
-
  background(0); 
-
+ image(video, 0, 0, 700, 600);
+ 
  fill("black");
  stroke("black");
  rect(680,0,20,700);
